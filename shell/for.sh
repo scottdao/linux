98b1e10e799aad06c8de8f9c4f7bf5931bace1f2
scottdao <13466484073@139.com>
2020-04-12 23:09:37 +0800

    add shell

---
+++ shell/for.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# 开启shell脚本的命令:chmod a+x   xxx.sh     2. xxx.sh
+
+# for((i=0; i<10; i++));
+# do
+#     echo $i;
+#     sleep 1;
+# done
+for(( a=0; a<10;a++));
+do
+    echo $a
+done
+
+echo 1
