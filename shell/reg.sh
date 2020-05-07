76f0e5424aa05bf0f305d1d6ff4330ea1be3bd57
scottdao <13466484073@139.com>
2020-05-07 09:10:11 +0800

    shell awk sed.ect tools

---
+++ shell/reg.sh
@@ -0,0 +1,16 @@
+
+# !/bin/bash
+
+# 正则表达式
+
+# 1. grep 'linux' file
+
+# 2. grep -c 'linux' file 有几行
+
+# 3. grep -n 'linux' file 有几行，并显示行号
+
+# 4. -i 忽略大小写； -v:取反，不包括字符行；复合:-ivn
+
+# 5. grep -E 'linux'; -E 表示写正则表达式；
+
+# 6. 
---
+++ shell/tool.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+# awk sed uniq sort split  五个工具：
+
+# 1. uniq
+
+# 2. sort -r/  file
+
+# 3. split 分割
+
+# 4. wc -l 统计出现次数； sort -k1
