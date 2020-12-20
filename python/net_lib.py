b771a6e28d128bb240c50eff820ac479860832f5
scottdao <13466484073@139.com>
2020-12-20 22:32:32 +0800

    add 添加spride

--- python/net_lib.py
+++ python/net_lib.py
@@ -1,14 +1,17 @@
 # 网络库
-### urllib库 http 协议常用库
-### requests 库 http协议常用库
-### BeautifulSoup库 xml格式处理库
+# urllib库 http 协议常用库
+# requests 库 http协议常用库
+# BeautifulSoup库 xml格式处理库
 
+from bs4 import BeautifulSoup
+import socket
+import urllib
 from urllib import request
 from urllib import parse
 
-data = bytes(parse.urlencode({'word':'hello'}), encoding = 'utf-8')
+data = bytes(parse.urlencode({'word': 'hello'}), encoding='utf-8')
 
-url  = 'http://www.baidu.com'
+url = 'http://www.baidu.com'
 
 response = request.urlopen('http://httpbin.org/post', data=data)
 
@@ -16,15 +19,12 @@ response = request.urlopen('http://httpbin.org/post', data=data)
 jg = response.read().decode('utf-8')
 # print(jg)
 
-import urllib
-
-import socket
 
 try:
-   res2 = request.urlopen('http://httpbin.org/get', timeout = 1)
-   jg2 = res2.read()
-   print(jg2)
+    res2 = request.urlopen('http://httpbin.org/get', timeout=1)
+    jg2 = res2.read()
+    soup = BeautifulSoup(jg2, 'lxml')
+#    print(jg2)
 except urllib.error.URLError as e:
     if isinstance(e.reason, socket.timeout):
         print('timeout')
-
---
+++ python/spride_test.py
@@ -0,0 +1,4 @@
+# 网络爬虫
+import requests
+content = requests.get('http://wwww.cnu.c/discoverPage/hot-人像').text
+img = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606824829870&di=4d2ed8bf144089f732b6699547950803&imgtype=0&src=http%3A%2F%2Fa2.att.hudong.com%2F27%2F81%2F01200000194677136358818023076.jpg'
